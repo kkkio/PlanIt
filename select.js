5f416be6d517cf28f93d8b8d54a66f8eec9848f5
Eveline Y Jiang <eveline0422@gmail.com>
2017-03-14 08:49:54 +0800

    add description

--- select.js
+++ select.js
@@ -1,3 +1,10 @@
+// retrieve data from database
+// need to change password for mysql and database names when insert in different database
+// change path of mysql temporarily
+// PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/mysql/bin
+// start mysql
+// mysql -h 127.0.0.1 -P 3306 -u root -p
+
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
