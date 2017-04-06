a568d97505ee08a068bdc727208c2ed85f75ee6a
kkkio <kevin.l.yide@gmail.com>
2017-04-07 03:06:16 +0800

    get register done

--- routes/users.js
+++ routes/users.js
@@ -241,22 +241,20 @@ function isCompleted(req, res, next){
   var email = req.body.email;
   var username = req.body.username;
   var password = req.body.password;
-  var password2 = req.body.password2;
+  var password_confirmation = req.body.password_confirmation;
 
   req.checkBody('email', 'Email is required').notEmpty();
   req.checkBody('email', 'Email is not valid').isEmail();
   req.checkBody('username', 'Username is required').notEmpty();
   req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+  req.checkBody('password_confirmation', 'Passwords do not match').equals(req.body.password);
 
 
   var errors = req.validationErrors();
 
   if(errors){
     console.log(errors);
-    res.render('register',{
-      errors:errors
-    });
+    return next();
   } else {
     console.log('PASSED');
     return next();
