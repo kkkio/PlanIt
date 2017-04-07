1c1ebb09c3d0d8c7c54fc0b6348d287c546d2d44
kkkio <kevin.l.yide@gmail.com>
2017-04-07 10:47:13 +0800

    index router add

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET login-homepage. */
+router.get('/', isLoggedIn,function(req, res, next) {
   res.render('index', {
-    title: 'Express',
-    projectname: 'PlanIt'
    });
 });
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.render('index_before_login',{
+    
+  });
+}
 
 module.exports = router;
