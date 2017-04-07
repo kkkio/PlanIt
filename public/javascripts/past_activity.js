66687b6501f4e1312de8abfc4a86f06587d841af
LilyLidl <lilylee19960516@gmail.com>
2017-04-07 20:08:48 +0800

    past activity

---
+++ public/javascripts/past_activity.js
@@ -0,0 +1,32 @@
+// Menu toggle script
+$(document).ready(function(){
+	$("#menu-toggle").click( function (e){
+   		e.preventDefault();
+   		$("#wrapper").toggleClass("menuDisplayed");
+	});
+});
+
+// Back to top
+$(document).ready(function(){
+   $(window).scroll(function () {
+   		if ($(this).scrollTop() > 50) {
+        	$('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+        	scrollTop: 0
+        }, 800);
+     	return false;
+    });
+        
+    $('#back-to-top').tooltip('show');
+
+});
+
+
+
