4792b25abb9e8d030a4727dc69bca6cb20167243
kkkio <kevin.l.yide@gmail.com>
2017-04-10 01:31:17 +0800

    update model & controller

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 
 var commentSchema = mongoose.Schema({
 	_activity_id : {type: mongoose.Schema.Types.ObjectId, ref: 'activity'},
+	_moment_id : {type: mongoose.Schema.Types.ObjectId, ref: 'moment'},
 	_user_id : {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
 	// type : acitivity - 1; moment - 2
 	content: String,
