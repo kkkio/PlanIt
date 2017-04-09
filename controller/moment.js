0b5683ab28cf39e725da4a60a53c9e804ecc8cae
josenzzc <zzczjq96@gmail.com>
2017-04-08 23:26:51 -0700

    send moment successfully to frontend

--- controller/moment.js
+++ controller/moment.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var moment = require('../models/moment');
 exports = module.exports = {};
-exports.test=function(){
-	console.log("test");	
-};
-exports.mymoment=function mymoment(req,res,next){
+
+exports.mymoment=function mymoment(req,res,next,callback){
 	var data=[];
+	console.log("in my moment");
 	console.log(req.user._id);
-	console.log("the id");
-	data=moment.showMyMoment(req.user._id);
-	return data;
+	
+	moment.showMyMoment(req.user._id,function(doc){
+		//console.log("in callback of mymoment");
+		console.log("doc==");
+		console.log(doc);
+		res.render('account', {
+			user : req.user,
+			data : doc
+		});	
+		console.log("finish rendering data");
+	});
+
+	
 };
 //add moment
-exports.amoment = function addmoment(req,res,next){
+exports.addMoment = function addmoment(req,res,next){
 	var insert_data={
     	title :  	req.body.title,
       	_user_id :	req.user._id,
@@ -27,27 +36,26 @@ exports.amoment = function addmoment(req,res,next){
     data.save();
 };
 
-exports.dmoment = function deletemoment(req,res,next){
-	var id=req.user._id;
+exports.deleteMoment = function deletemoment(req,res,next){
+	var id=req.body.id;
     Schedule.findByIdAndRemove(id).exec();
 };
 
-exports.umoment = function updatemoment(req,res,next){
-	var id = req.user._id;
+exports.updateMoment = function updatemoment(req,res,next){
+	var id = req.body.id;
   	moment.findById(id, function(err, doc) {
     if (err) {
       	console.error('error, no entry found');
     }
 	else{
 		doc.title  = req.body.title;
-		doc.text=req.body.text;
+		doc.text = req.body.text;
 		doc.save();
 	}
 });
 };
 
-exports.fmoment = function friendmoment(req,res,next){
+exports.friendMoment = function friendmoment(req,res,next){
 	var friend_id=req.params.id;
 	var data=moment.showFriendMoment(friend_id);
-    res.render('othersmoment',data);
 };
