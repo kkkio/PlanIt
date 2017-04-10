468412d8d052b3133d4780ef035c93402ddac92a
kkkio <kevin.l.yide@gmail.com>
2017-04-10 14:01:14 +0800

    get activity

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.use(function (req, res, next) {
 app.use('/', index);
 app.use('/users', users);
 app.use('/explore',explore);
-//app.use('/activity',activity);
+app.use('/activity',activity);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
