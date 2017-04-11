97c9a77b2ca4be60c35fecbf3cc08438161b848c
josenzzc <zzczjq96@gmail.com>
2017-04-11 01:23:48 -0700

    easy explore routers for liliil test

--- routes/explore.js
+++ routes/explore.js
@@ -16,21 +16,25 @@ router.get('/search', isLoggedIn,function(req, res, next) {
    });
 });
 //for lilili test easy router search navBar
-router.get('/explore/sports',isLoggedIn,function(req,res,next){
+router.get('/sports',isLoggedIn,function(req,res,next){
 	res.render('explore_sports');
 });
-router.get('/explore/tech',isLoggedIn,function(req,res,next){
+router.get('/tech',isLoggedIn,function(req,res,next){
 	res.render('explore_tech');
 });
-router.get('/explore/movies',isLoggedIn,function(req,res,next){
+router.get('/movies',isLoggedIn,function(req,res,next){
 	res.render('explore_movies');
 });
-router.get('/explore/other',isLoggedIn,function(req,res,next){
+router.get('/other',isLoggedIn,function(req,res,next){
 	res.render('explore_other');
 });
-router.get('/explore/Culture',isLoggedIn,function(req,res,next){
+router.get('/Culture',isLoggedIn,function(req,res,next){
 	res.render('explore_Culture');
 });
+router.get('/music',isLoggedIn,function(req,res,next){
+	res.render('explore_music');
+});
+
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
