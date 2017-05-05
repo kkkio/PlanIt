98ada20cc58b91f1b5226c30f10073d0f192f610
kkkio <kevin.l.yide@gmail.com>
2017-05-05 22:00:00 +0800

    Add config.js to .gitignore; add example_config.js

---
+++ config/example_config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    db: '**REMOVED**',
+    facebook: {
+			clientID: "**REMOVED**",
+      clientSecret: "**REMOVED**",
+      callbackURL: "**REMOVED**"
+		},
+
+		google: {
+			clientID: "**REMOVED**",
+			clientSecret: "**REMOVED**",
+			callbackURL: "**REMOVED**"
+		},
+    gmailTransport:{
+      service : '**REMOVED**',
+      username: '**REMOVED**',
+      password: '**REMOVED**'
+    },
+    greetingEmail:{
+
+    }
+}
